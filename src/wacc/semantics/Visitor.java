7a6bcc081c2465187f3fb4c64b0b33a7f9f4447b
Tom Barwick <tb1414@ic.ac.uk>
2015-11-13 04:14:26 +0000

    Sets out semantic checker outline for all functions and details what each function needs to do. This has improved clarity in how to proceed with the semantic checker

--- src/wacc/semantics/Visitor.java
+++ src/wacc/semantics/Visitor.java
@@ -1,217 +1,176 @@
 package wacc.semantics;
 
-import org.antlr.v4.runtime.misc.NotNull;
-import org.antlr.v4.runtime.tree.ParseTree;
-
-import wacc.antlr.*;
-import wacc.symbolTable.ScopeHandler;
+import wacc.antlr.WACCParser.*;
+import wacc.antlr.WACCParserBaseVisitor;
 
 public class Visitor extends WACCParserBaseVisitor<Void> {
 	
-	ScopeHandler scopeHandler = new ScopeHandler();
+	private String nodeType = "null";
+	
+	/*
+	 * Visits stats
+	 */
+
+	@Override
+	public Void visitInitialisation(InitialisationContext ctx) {
+		// TODO Add variable to current scope
+		// TODO Check RHS type is equal to variable type
+		return super.visitInitialisation(ctx);
+	}
+
+	@Override
+	public Void visitAssignment(AssignmentContext ctx) {
+		// TODO Check LHS type = RHS type
+		return super.visitAssignment(ctx);
+	}
+
+	@Override
+	public Void visitRead(ReadContext ctx) {
+		// TODO Check type is = program variable | pair elem | array elem
+		// TODO Type must be either int, string(?) or char
+		return super.visitRead(ctx);
+	}
+
+	@Override
+	public Void visitFree(FreeContext ctx) {
+		// TODO Type must be either array or pair
+		// TODO Type must not be an array of an array or an array of a pair or a pair of arrays etc.
+		return super.visitFree(ctx);
+	}
 	
 	@Override
-	public Void visitProgram(@NotNull WACCParser.ProgramContext ctx) {
-		return visitChildren(ctx);
+	public Void visitExit(ExitContext ctx) {
+		// TODO Expression must evaluate to an int
+		return super.visitExit(ctx);
+	}
+
+	@Override
+	public Void visitIf(IfContext ctx) {
+		// TODO Check condition is a valid boolean or evaluates to one
+		// TODO Increase symtab scope before visiting EACH child individually
+		// TODO Reduce symtab scope after visiting EACH child individually
+		return super.visitIf(ctx);
+	}
+
+	@Override
+	public Void visitWhile(WhileContext ctx) {
+		// TODO Check condition is a valid boolean or evaluates to one
+		// TODO Increase symtab scope before visiting child
+		// TODO Reduce symtab scope afterwards
+		return super.visitWhile(ctx);
 	}
 	
 	@Override
-	public Void visitFunc(@NotNull WACCParser.FuncContext ctx) {
-		String ident = ctx.getChild(1).getText();
-		String type = ctx.getChild(0).getText();
-		try {
-			scopeHandler.add(ident, type);
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
-		return visitChildren(ctx);
+	public Void visitReturn(ReturnContext ctx) {
+		// TODO Expression must be same type as function return type
+		return super.visitReturn(ctx);
+	}
+	
+	/*
+	 * Visits expressions
+	 */
+	
+	@Override
+	public Void visitInt(IntContext ctx) {
+		nodeType = "int";
+		return super.visitInt(ctx);
+	}
+	
+	@Override
+	public Void visitBool(BoolContext ctx) {
+		nodeType = "bool";
+		return super.visitBool(ctx);
+	}
+	
+	@Override
+	public Void visitChar(CharContext ctx) {
+		nodeType = "char";
+		return super.visitChar(ctx);
 	}
 
 	@Override
-	public Void visitParam(@NotNull WACCParser.ParamContext ctx) {
-		String ident = ctx.getChild(1).getText();
-		String type = ctx.getChild(0).getText();
-		try {
-			scopeHandler.add(ident, type);
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
-		return visitChildren(ctx);
+	public Void visitString(StringContext ctx) {
+		nodeType = "string";
+		return super.visitString(ctx);
 	}
 
 	@Override
-	public Void visitBinary_op(@NotNull WACCParser.Binary_opContext ctx) {
-		return visitChildren(ctx);
+	public Void visitPair(PairContext ctx) {
+		nodeType = "pair";
+		return super.visitPair(ctx);
 	}
 
 	@Override
-	public Void visitArray_lit(@NotNull WACCParser.Array_litContext ctx) {
-		return visitChildren(ctx);
+	public Void visitArrayElem(ArrayElemContext ctx) {
+		// TODO check exp evaluates to a positive int
+		return super.visitArrayElem(ctx);
 	}
 
 	@Override
-	public Void visitSkip(@NotNull WACCParser.SkipContext ctx) {
-		return visitChildren(ctx);
+	public Void visitIdentExp(IdentExpContext ctx) {
+		// TODO Make sure identifier exists in current scope or higher
+		return super.visitIdentExp(ctx);
+	}
+	
+	@Override
+	public Void visitUnaryOpExp(UnaryOpExpContext ctx) {
+		// TODO Check expressions match types allowed by specified unary operator
+		// TODO set nodeType = return type
+		// '!' 		allows bool		returns bool
+		// '-' 		allows int		returns int
+		// 'len' 	allows arrays	returns int
+		// 'ord' 	allows char		returns int
+		// 'chr' 	allows int		returns char
+		return super.visitUnaryOpExp(ctx);
 	}
 
 	@Override
-	public Void visitWhile(@NotNull WACCParser.WhileContext ctx) {
-		return visitChildren(ctx);
+	public Void visitBinaryOpExp(BinaryOpExpContext ctx) {
+		// TODO Check LHS and RHS have same types
+		// TODO set nodeType = return type
+		// *, /, %, +, - take int return int
+		// >, >=, <, <= take int/char return bool
+		// ==, != take anything return bool
+		// &&, || take bool return bool
+		return super.visitBinaryOpExp(ctx);
+	}
+	
+	/*
+	 * Visit other rules
+	 */
+
+	@Override
+	public Void visitFunc(FuncContext ctx) {
+		// TODO Increase symtab scope, visit children, then decrease symtab scope
+		// TODO Possibly add parameters to a global function signature tracker
+		// TODO Check every path of execut1ion contains a return statement
+		return super.visitFunc(ctx);
 	}
 
 	@Override
-	public Void visitFree(@NotNull WACCParser.FreeContext ctx) {
-		System.out.println("Visiting 'Free' node");
-		return visitChildren(ctx);
+	public Void visitParam(ParamContext ctx) {
+		// TODO Add param to current scope
+		return super.visitParam(ctx);
+	}
+	
+	@Override
+	public Void visitArray_lit(Array_litContext ctx) {
+		// TODO Check all children have same type
+		// TODO nodeType = CHILDTYPE + "[]"
+		return super.visitArray_lit(ctx);
+	}
+	
+	@Override
+	public Void visitPair_elem(Pair_elemContext ctx) {
+		// TODO Visit child and obtain type
+		// nodeType = CHILDTYPE
+		return super.visitPair_elem(ctx);
 	}
 
 	@Override
-	public Void visitIdent(@NotNull WACCParser.IdentContext ctx) {
-		System.out.println("Visiting 'Ident' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitPrintln(@NotNull WACCParser.PrintlnContext ctx) {
-		System.out.println("Visiting 'Println' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitInitialisation(@NotNull WACCParser.InitialisationContext ctx) {
-		System.out.println("Visiting 'Initialisation' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitIf(@NotNull WACCParser.IfContext ctx) {
-		System.out.println("Visiting 'If' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitRead(@NotNull WACCParser.ReadContext ctx) {
-		System.out.println("Visiting 'Read' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitArray_type(@NotNull WACCParser.Array_typeContext ctx) {
-		System.out.println("Visiting 'Array_type' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitPair_elem(@NotNull WACCParser.Pair_elemContext ctx) {
-		System.out.println("Visiting 'Pair_elem' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitReturn(@NotNull WACCParser.ReturnContext ctx) {
-		System.out.println("Visiting 'Return' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitNumber(@NotNull WACCParser.NumberContext ctx) {
-		System.out.println("Visiting 'Number' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitBase_type(@NotNull WACCParser.Base_typeContext ctx) {
-		System.out.println("Visiting 'Base_type' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitPair_type(@NotNull WACCParser.Pair_typeContext ctx) {
-		System.out.println("Visiting 'Pair_type' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitAssign_lhs(@NotNull WACCParser.Assign_lhsContext ctx) {
-		System.out.println("Visiting 'Assign_lhs' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitPrint(@NotNull WACCParser.PrintContext ctx) {
-		System.out.println("Visiting 'Print' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitExit(@NotNull WACCParser.ExitContext ctx) {
-		System.out.println("Visiting 'Exit' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitParam_list(@NotNull WACCParser.Param_listContext ctx) {
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitBegin(@NotNull WACCParser.BeginContext ctx) {
-		System.out.println("Visiting 'Begin' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitInt_lit(@NotNull WACCParser.Int_litContext ctx) {
-		System.out.println("Visiting 'Int Lit' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitExp(@NotNull WACCParser.ExpContext ctx) {
-		System.out.println("Visiting 'Exp' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitArg_list(@NotNull WACCParser.Arg_listContext ctx) {
-		System.out.println("Visiting 'Arg List' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitAssignment(@NotNull WACCParser.AssignmentContext ctx) {
-		System.out.println("Visiting 'Assignment' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitArray_elem(@NotNull WACCParser.Array_elemContext ctx) {
-		System.out.println("Visiting 'Array Elem' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitSequence(@NotNull WACCParser.SequenceContext ctx) {
-		System.out.println("Visiting 'Sequence' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitPair_elem_type(@NotNull WACCParser.Pair_elem_typeContext ctx) {
-		System.out.println("Visiting 'Pair Elem Type' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitAssign_rhs(@NotNull WACCParser.Assign_rhsContext ctx) {
-		System.out.println("Visiting 'Assign Rhs' node");
-		return visitChildren(ctx);
-	}
-
-	@Override
-	public Void visitUnary_op(@NotNull WACCParser.Unary_opContext ctx) {
-		System.out.println("Visiting 'Unary Op' node");
-		return visitChildren(ctx);
+	public Void visitAssign_rhs(Assign_rhsContext ctx) {
+		// TODO When calling functions, check arg types are equal function signature types
+		return super.visitAssign_rhs(ctx);
 	}
 
 }
