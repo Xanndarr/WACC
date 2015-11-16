654beedaa48b78fddcbe4b4209d65e66d80f4cf8
Tom Barwick <tb1414@ic.ac.uk>
2015-11-16 05:58:37 +0000

    Adds a function handler to keep track of functions, their types, their params and their param types

---
+++ src/wacc/symbolTable/FunctionHandler.java
@@ -0,0 +1,41 @@
+package wacc.symbolTable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FunctionHandler {
+	
+	private Map<String, SymbolTable> functions;
+	private final String FUNCTION_TYPE = "$TYPE";
+	
+	public FunctionHandler() {
+		functions = new HashMap<String, SymbolTable>();
+	}
+	
+	public void add(String ident, String returnType) {
+		SymbolTable params = new SymbolTable();
+		params.put(FUNCTION_TYPE, returnType);
+		functions.put(ident, params);
+	}
+	
+	public void addParam(String ident, String paramName, String paramType) {
+		functions.get(ident).put(paramName, paramType);
+	}
+	
+	public String getReturnType(String ident) {
+		return functions.get(ident).get(FUNCTION_TYPE);
+	}
+	
+	public String getParamType(String functionIdent, String paramIdent) {
+		return functions.get(functionIdent).get(paramIdent);
+	}
+	
+	public boolean exists(String ident) {
+		return functions.containsKey(ident);
+	}
+	
+	public boolean existsParam(String functionIdent, String paramIdent) {
+		return functions.containsKey(functionIdent) && functions.get(functionIdent).exists(paramIdent);
+	}
+
+}
