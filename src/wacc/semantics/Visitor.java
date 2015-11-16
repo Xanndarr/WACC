d7f2685fe3b67fed390bc517f7bcf70d71353fd2
Tom Barwick <tb1414@ic.ac.uk>
2015-11-16 06:06:22 +0000

    Reformats coding style using standard formatter

--- src/wacc/semantics/Visitor.java
+++ src/wacc/semantics/Visitor.java
@@ -7,11 +7,11 @@ import wacc.symbolTable.ScopeHandler;
 import wacc.antlr.WACCParserBaseVisitor;
 
 public class Visitor extends WACCParserBaseVisitor<Void> {
-	
+
 	private ScopeHandler scopeHandler = new ScopeHandler();
 	private FunctionHandler functionHandler = new FunctionHandler();
 	private String nodeType = "null";
-	
+
 	/*
 	 * Visits stats
 	 */
@@ -28,13 +28,13 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 		} else {
 			System.err.println("Error: The variable '" + ident + "' already exists.");
 		}
-		
+
 		visit(ctx.assign_rhs());
-		//if (!nodeType.equals(type)) { // hacked below
+		// if (!nodeType.equals(type)) { // hacked below
 		if (!type.contains(nodeType)) {
 			// Set exit status to 200 ? Not sure how to do this.
-			System.err.println("Error: Incompatible type at ' " + ctx.assign_rhs().getText() +
-			" ' (Expected: " + type + ", Actual: " + nodeType + ")");
+			System.err.println("Error: Incompatible type at ' " + ctx.assign_rhs().getText() + " ' (Expected: " + type
+					+ ", Actual: " + nodeType + ")");
 		}
 
 		return super.visitInitialisation(ctx);
@@ -45,66 +45,61 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 		// TODO Check LHS type = RHS type
 		// TODO Check that idents exist
 		System.out.println("Visiting assignment: " + ctx.getText());
-		
-		
+
 		// LHS
 		Assign_lhsContext lhs = ctx.assign_lhs();
 		String lhsType = "";
-		
+
 		IdentContext ident = lhs.ident();
 		if (ident != null) {
 			visit(ident);
-//			boolean exists = scopeHandler.exists(ident.getText());
+			// boolean exists = scopeHandler.exists(ident.getText());
 			if (!scopeHandler.exists(ident.getText())) {
-				System.err.println("Error: Variable " + ident.getText() +
-						" does not exist in the current scope");
+				System.err.println("Error: Variable " + ident.getText() + " does not exist in the current scope");
 			} else {
-//				visit(ident);
+				// visit(ident);
 				System.out.println("Context: " + lhs.getText());
 				lhsType = nodeType;
 				System.out.println("Type: " + lhsType);
 			}
 		}
-		
+
 		Array_elemContext array_elem = lhs.array_elem();
 		if (array_elem != null) {
-			//System.out.println("Context: " + lhs.getText());
+			// System.out.println("Context: " + lhs.getText());
 			boolean exists = scopeHandler.exists(array_elem.ident().getText());
 			if (!exists) {
-				System.err.println("Error: Variable " + array_elem.ident().getText() +
-						" does not exist in the current scope");
+				System.err.println(
+						"Error: Variable " + array_elem.ident().getText() + " does not exist in the current scope");
 			} else {
 				visit(array_elem);
 				lhsType = nodeType;
 			}
 		}
-		
+
 		Pair_elemContext pair_elem = lhs.pair_elem();
 		if (pair_elem != null) {
 			boolean exists = scopeHandler.exists(pair_elem.exp().getText());
 			if (!exists) {
-				System.err.println("Error: Variable " + pair_elem.exp().getText() +
-						" does not exist in the current scope");
+				System.err.println(
+						"Error: Variable " + pair_elem.exp().getText() + " does not exist in the current scope");
 			} else {
 				visit(pair_elem);
 				lhsType = nodeType;
 			}
 		}
-		
-		
+
 		// RHS
 		visit(ctx.assign_rhs());
 		String rhsType = nodeType;
 		if (!lhs.isEmpty()) {
 			if (!rhsType.equals(lhsType)) {
-				System.err.println("Error: Incompatible type at ' " + ctx.assign_rhs().getText() +
-						" ' (Expected: " + lhsType + ", Actual: " + rhsType + ")");
+				System.err.println("Error: Incompatible type at ' " + ctx.assign_rhs().getText() + " ' (Expected: "
+						+ lhsType + ", Actual: " + rhsType + ")");
 			}
 		}
 		return super.visitAssignment(ctx);
 	}
-
-
 
 	@Override
 	public Void visitRead(ReadContext ctx) {
@@ -116,10 +111,11 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 	@Override
 	public Void visitFree(FreeContext ctx) {
 		// TODO Type must be either array or pair
-		// TODO Type must not be an array of an array or an array of a pair or a pair of arrays etc.
+		// TODO Type must not be an array of an array or an array of a pair or a
+		// pair of arrays etc.
 		return super.visitFree(ctx);
 	}
-	
+
 	@Override
 	public Void visitExit(ExitContext ctx) {
 		// DONE Expression must evaluate to an int
@@ -152,11 +148,11 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 		// DONE Check condition is a valid boolean or evaluates to one
 		// DONE Increase symtab scope before visiting child
 		// DONE Reduce symtab scope afterwards
-		//System.out.println("visiting while");
-		//System.out.println(ctx.exp().getText());
+		// System.out.println("visiting while");
+		// System.out.println(ctx.exp().getText());
 		visit(ctx.exp());
 		if (!nodeType.equals("bool")) {
-			//System.out.println(nodeType);
+			// System.out.println(nodeType);
 			System.err.println("Error: While condition expressions must evaluate to bool type.");
 		}
 		scopeHandler.descend();
@@ -164,37 +160,37 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 		scopeHandler.ascend();
 		return super.visitWhile(ctx);
 	}
-	
+
 	@Override
 	public Void visitReturn(ReturnContext ctx) {
 		// TODO Expression must be same type as function return type
 		System.out.println("Visiting return");
 		visit(ctx.exp());
 		String returnType = nodeType;
-		String functionType = ((FuncContext)ctx.getParent()).type().getText();
+		String functionType = ((FuncContext) ctx.getParent()).type().getText();
 		if (!returnType.equals(functionType)) {
-			System.err.println("Error: Incompatible type at '" + ctx.getText() +
-					"' (Expected: " + functionType + ", Actual: " + returnType + ")");
+			System.err.println("Error: Incompatible type at '" + ctx.getText() + "' (Expected: " + functionType
+					+ ", Actual: " + returnType + ")");
 		}
 		return super.visitReturn(ctx);
 	}
-	
+
 	/*
 	 * Visits expressions
 	 */
-	
+
 	@Override
 	public Void visitInt(IntContext ctx) {
 		nodeType = "int";
 		return super.visitInt(ctx);
 	}
-	
+
 	@Override
 	public Void visitBool(BoolContext ctx) {
 		nodeType = "bool";
 		return super.visitBool(ctx);
 	}
-	
+
 	@Override
 	public Void visitChar(CharContext ctx) {
 		nodeType = "char";
@@ -212,8 +208,8 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 		nodeType = "pair";
 		return super.visitPair(ctx);
 	}
-	
-	//This method is visited when a lookup into an array is being done
+
+	// This method is visited when a lookup into an array is being done
 	@Override
 	public Void visitArray_elem(Array_elemContext ctx) {
 		// DONE check exp evaluates to a (positive) int
@@ -234,37 +230,43 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 		}
 		return super.visitIdent(ctx);
 	}
-	
+
 	@Override
 	public Void visitUnaryOpExp(UnaryOpExpContext ctx) {
-		// DONE Check expressions match types allowed by specified unary operator
+		// DONE Check expressions match types allowed by specified unary
+		// operator
 		// DONE set nodeType = return type
-		// '!' 		allows bool		returns bool
-		// '-' 		allows int		returns int
-		// 'len' 	allows arrays	returns int
-		// 'ord' 	allows char		returns int
-		// 'chr' 	allows int		returns char
+		// '!' allows bool returns bool
+		// '-' allows int returns int
+		// 'len' allows arrays returns int
+		// 'ord' allows char returns int
+		// 'chr' allows int returns char
 		visit(ctx.exp());
 		switch (ctx.unary_op().getText()) {
 		case "!":
-			if (!nodeType.equals("bool")) System.err.println("Error: The '!' operator only works for boolean types.");
+			if (!nodeType.equals("bool"))
+				System.err.println("Error: The '!' operator only works for boolean types.");
 			nodeType = "bool";
 			break;
 		case "-":
-			if (!nodeType.equals("int")) System.err.println("Error: The '-' operator only works for integer types.");
+			if (!nodeType.equals("int"))
+				System.err.println("Error: The '-' operator only works for integer types.");
 			nodeType = "int";
 			break;
 		case "len":
 			// TODO check node type is of array
-			if (!nodeType.equals("ARRAYS?!?!")) System.err.println("Error: The 'len' operator only works for array types.");
+			if (!nodeType.equals("ARRAYS?!?!"))
+				System.err.println("Error: The 'len' operator only works for array types.");
 			nodeType = "int";
 			break;
 		case "ord":
-			if (!nodeType.equals("char")) System.err.println("Error: The 'ord' operator only works for char types.");
+			if (!nodeType.equals("char"))
+				System.err.println("Error: The 'ord' operator only works for char types.");
 			nodeType = "int";
 			break;
 		case "chr":
-			if (!nodeType.equals("int")) System.err.println("Error: The 'chr' operator only works for int types.");
+			if (!nodeType.equals("int"))
+				System.err.println("Error: The 'chr' operator only works for int types.");
 			nodeType = "char";
 			break;
 		default:
@@ -274,119 +276,120 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 		return super.visitUnaryOpExp(ctx);
 	}
 
-	 @Override
-	  public Void visitBinaryOpExp(BinaryOpExpContext ctx) {
-	    // DONE Check LHS and RHS have same types
-	    // DONE set nodeType = return type
-	    // *, /, %, +, - take int return int
-	    // >, >=, <, <= take int/char return bool
-	    // ==, != take anything return bool
-	    // &&, || take bool return bool
-		 ExpContext lhs = ctx.exp(0);
-		 ExpContext rhs = ctx.exp(1);
-	    if (!scopeHandler.exists(lhs.getText()) || !scopeHandler.exists(rhs.getText())) {
-	      System.err.println("Error: undeclared variable");
-	    }
-	    visit(lhs);
-	    String lhsType = nodeType;
-	    visit(rhs);
-	    String rhsType = nodeType;
+	@Override
+	public Void visitBinaryOpExp(BinaryOpExpContext ctx) {
+		// DONE Check LHS and RHS have same types
+		// DONE set nodeType = return type
+		// *, /, %, +, - take int return int
+		// >, >=, <, <= take int/char return bool
+		// ==, != take anything return bool
+		// &&, || take bool return bool
+		ExpContext lhs = ctx.exp(0);
+		ExpContext rhs = ctx.exp(1);
+		if (!scopeHandler.exists(lhs.getText()) || !scopeHandler.exists(rhs.getText())) {
+			System.err.println("Error: undeclared variable");
+		}
+		visit(lhs);
+		String lhsType = nodeType;
+		visit(rhs);
+		String rhsType = nodeType;
 
-	    if (!lhsType.equals(rhsType)) {
-	      System.err
-	          .println("Error: Both sides of a binary operator must have the same type.");
-	    }
+		if (!lhsType.equals(rhsType)) {
+			System.err.println("Error: Both sides of a binary operator must have the same type.");
+		}
 
-	    switch (ctx.binary_op().getText()) {
-	    case "*":
-	    case "/":
-	    case "%":
-	    case "+":
-	    case "-":
-	      if (!lhs.equals("int"))
-	        System.err.println("Error: *, /, %, +, - require ints.");
-	      nodeType = "int";
-	      break;
-	    case ">":
-	    case ">=":
-	    case "<":
-	    case "<=":
-	      if (!lhs.equals("int") && !lhs.equals("char"))
-	        System.err.println("Error: >, >=, <, <= require ints or chars.");
-	      nodeType = "bool";
-	      break;
-	    case "==":
-	    case "!=":
-	      nodeType = "bool";
-	      break;
-	    case "&&":
-	    case "||":
-	      if (!lhs.equals("bool"))
-	        System.err.println("Error: &&, || require bools.");
-	      nodeType = "bool";
-	      break;
-	    default:
-	      nodeType = "null";
-	      break;
-	    }
-	    return super.visitBinaryOpExp(ctx);
-	  }
-	
+		switch (ctx.binary_op().getText()) {
+		case "*":
+		case "/":
+		case "%":
+		case "+":
+		case "-":
+			if (!lhs.equals("int"))
+				System.err.println("Error: *, /, %, +, - require ints.");
+			nodeType = "int";
+			break;
+		case ">":
+		case ">=":
+		case "<":
+		case "<=":
+			if (!lhs.equals("int") && !lhs.equals("char"))
+				System.err.println("Error: >, >=, <, <= require ints or chars.");
+			nodeType = "bool";
+			break;
+		case "==":
+		case "!=":
+			nodeType = "bool";
+			break;
+		case "&&":
+		case "||":
+			if (!lhs.equals("bool"))
+				System.err.println("Error: &&, || require bools.");
+			nodeType = "bool";
+			break;
+		default:
+			nodeType = "null";
+			break;
+		}
+		return super.visitBinaryOpExp(ctx);
+	}
+
 	/*
 	 * Visit other rules
 	 */
 
 	@Override
 	public Void visitFunc(FuncContext ctx) {
-		// DONE Increase symtab scope, visit children, then decrease symtab scope
+		// DONE Increase symtab scope, visit children, then decrease symtab
+		// scope
 		// DONE Possibly add parameters to a global function signature tracker
 		// TODO Check every path of execution contains a return statement
 		String functionIdent = ctx.ident().getText();
 		String functionType = ctx.type().getText();
-		
+
 		if (!functionHandler.exists(functionIdent)) {
 			functionHandler.add(functionIdent, functionType);
 		} else {
 			System.err.println("Error: Function '" + functionIdent + "' already exists.");
 		}
-		
+
 		scopeHandler.descend();
-		
+
 		for (ParamContext param : ctx.param_list().param()) {
 			String paramIdent = param.ident().getText();
 			String paramType = param.type().getText();
-			
+
 			if (!functionHandler.existsParam(functionIdent, paramIdent)) {
 				functionHandler.addParam(functionIdent, paramIdent, paramType);
 			} else {
-				System.err.println("Error: Parameter with name '" + paramIdent + "' alerady exists within this function definition. ");
+				System.err.println("Error: Parameter with name '" + paramIdent
+						+ "' alerady exists within this function definition. ");
 			}
 			scopeHandler.add(paramIdent, paramType);
 		}
-		
+
 		visit(ctx.stat());
 		scopeHandler.ascend();
-		
+
 		return super.visitFunc(ctx);
 	}
-	
+
 	@Override
 	public Void visitArray_lit(Array_litContext ctx) {
 		// TODO Check all children have same type
 		// TODO nodeType = CHILDTYPE + "[]"
 		System.out.println("Visiting array_lit");
-		
+
 		nodeType = "[]";
-		
+
 		if (ctx.exp().size() > 0) {
 			visit(ctx.exp(0));
 			String type = nodeType;
 			System.out.println("Type: " + type);
 			for (int i = 1; i < ctx.exp().size(); i++) {
 				visit(ctx.exp(i));
-				if(!nodeType.equals(type)) {
-					System.err.println("Error: Array element types must be the same, at '" +
-					ctx.exp(i).getText() + "' Expected: " + type + ", Actual: " + nodeType);
+				if (!nodeType.equals(type)) {
+					System.err.println("Error: Array element types must be the same, at '" + ctx.exp(i).getText()
+							+ "' Expected: " + type + ", Actual: " + nodeType);
 				}
 			}
 			nodeType = type + "[]";
@@ -394,7 +397,7 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 		System.out.println("Final node tpye at array_lit; " + nodeType);
 		return super.visitArray_lit(ctx);
 	}
-	
+
 	@Override
 	public Void visitPair_elem(Pair_elemContext ctx) {
 		// TODO Visit child and obtain type
@@ -406,7 +409,8 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 
 	@Override
 	public Void visitAssign_rhs(Assign_rhsContext ctx) {
-		// TODO When calling functions, check arg types are equal function signature types
+		// TODO When calling functions, check arg types are equal function
+		// signature types
 		return super.visitAssign_rhs(ctx);
 	}
 
