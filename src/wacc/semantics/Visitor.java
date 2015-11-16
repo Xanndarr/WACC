40538746fe3936616325ba2a9c1d4dfeb0bfcce4
Glen Rodgers <gr1614@ic.ac.uk>
2015-11-16 21:52:51 +0000

    Changed a typo in a variable in the binary_op semantic rule

--- src/wacc/semantics/Visitor.java
+++ src/wacc/semantics/Visitor.java
@@ -324,6 +324,7 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 		// &&, || take bool return bool
 		ExpContext lhs = ctx.exp(0);
 		ExpContext rhs = ctx.exp(1);
+		System.out.println("lhs: " + lhs.getText() + ", rhs: " + rhs.getText());
 		if (!scopeHandler.exists(lhs.getText()) || !scopeHandler.exists(rhs.getText())) {
 			System.err.println("Error: Undeclared variable: '" + lhs.getText() + "'." + rhs.getText());
 		}
@@ -342,7 +343,7 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 		case "%":
 		case "+":
 		case "-":
-			if (!lhs.equals("int"))
+			if (!lhsType.equals("int"))
 				System.err.println("Error: *, /, %, +, - require ints.");
 			nodeType = "int";
 			break;
@@ -350,7 +351,7 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 		case ">=":
 		case "<":
 		case "<=":
-			if (!lhs.equals("int") && !lhs.equals("char"))
+			if (!lhsType.equals("int") && !lhs.equals("char"))
 				System.err.println("Error: >, >=, <, <= require ints or chars.");
 			nodeType = "bool";
 			break;
@@ -360,7 +361,7 @@ public class Visitor extends WACCParserBaseVisitor<Void> {
 			break;
 		case "&&":
 		case "||":
-			if (!lhs.equals("bool"))
+			if (!lhsType.equals("bool"))
 				System.err.println("Error: &&, || require bools.");
 			nodeType = "bool";
 			break;
