dc956d51f3ea1f113d928d755d363f832b583c76
Tom Barwick <tb1414@ic.ac.uk>
2015-12-01 16:00:29 +0000

    Changes error reporter to make calling it more simple

--- src/wacc/ErrorReporter.java
+++ src/wacc/ErrorReporter.java
@@ -2,18 +2,32 @@ package wacc;
 
 import java.io.PrintStream;
 
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.Token;
 
 public class ErrorReporter {
 	
 	private PrintStream out = null;
+	private int errorCode = 0;
 	
 	public ErrorReporter(PrintStream out) {
 		this.out = out;
 	}
 	
-	public void println(String error, Token token) {
-		out.println(token.getLine() + ":" + token.getCharPositionInLine() + " " + error);
+	public void println(String error, ParserRuleContext ctx) {
+		Token token = ctx.getStart();
+		out.println(token.getLine() + ":" + token.getCharPositionInLine() +" Semantic Error: " + error);
+		errorCode = 200;
+	}
+	
+	public void printlnSyntax(String error, ParserRuleContext ctx) {
+		Token token = ctx.getStart();
+		out.println(token.getLine() + ":" + token.getCharPositionInLine() +" Syntax Error: " + error);
+		errorCode = 100;
+	}
+	
+	public int getReturnCode() {
+		return errorCode;
 	}
 
 }
