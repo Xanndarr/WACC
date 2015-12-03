253df920ee7bc1025468b510548b78bad46fb70d
Tom Barwick <tb1414@ic.ac.uk>
2015-12-03 23:48:24 +0000

    Adds enum to identify FST and SND of pairs

--- src/wacc/tree/nodes/PairElemNode.java
+++ src/wacc/tree/nodes/PairElemNode.java
@@ -5,7 +5,17 @@ import wacc.tree.nodeInterfaces.AssignRHSNode;
 import wacc.tree.nodeInterfaces.Node;
 
 public class PairElemNode implements AssignLHSNode, AssignRHSNode {
-
+	
+	public enum PairPos {
+		FST, SND;
+	}
+	
+	private PairPos pos;
+	
+	public PairElemNode(PairPos pos) {
+		this.pos = pos;
+	}
+	
     @Override
     public void generate() {
 
