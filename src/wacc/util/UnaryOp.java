2b167054fae0aea72a75a2abf4fbda6a9364c9a3
Tom Barwick <tb1414@ic.ac.uk>
2015-12-03 19:52:57 +0000

    Adds enums for unary and binary operators

---
+++ src/wacc/util/UnaryOp.java
@@ -0,0 +1,22 @@
+package wacc.util;
+
+public enum UnaryOp {
+	
+	ORD("ord"), CHR("chr"), NOT("!"), LEN("len"), SUB("-"), INVALID_OP("");
+
+	private String op;
+
+	UnaryOp(String op) {
+		this.op = op;
+	}
+	
+	public UnaryOp parseOp(String op) {
+		for (UnaryOp oper : UnaryOp.values()) {
+			if (oper.op.equals(op)) {
+				return oper;
+			}
+		}
+		return INVALID_OP;
+	}
+
+}
