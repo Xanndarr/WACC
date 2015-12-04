5de4d42514e86b36e099518806512f4cece21731
Tom Barwick <tb1414@ic.ac.uk>
2015-12-04 18:59:53 +0000

    Implements assembly writer

--- src/wacc/codeGen/AssemblyWriter.java
+++ src/wacc/codeGen/AssemblyWriter.java
@@ -1,5 +1,28 @@
 package wacc.codeGen;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import wacc.util.ProgramCode;
+
 public class AssemblyWriter {
+	
+	private String fileName;
+	
+	public AssemblyWriter(String fileName) {
+		this.fileName = fileName + ".s";
+	}
+	
+	public void write() throws IOException {
+		File output = new File(fileName);
+		PrintStream outStream = new PrintStream(output);
+		if (!output.exists()) {
+			output.createNewFile();
+		}
+		String code = ProgramCode.getCode();
+		outStream.print(code);
+		outStream.close();
+	}
 
 }
