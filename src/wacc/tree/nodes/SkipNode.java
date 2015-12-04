ac96de7d5d4dfd9741ee10ea6c88e2a8eba9142e
Tom Barwick <tb1414@ic.ac.uk>
2015-12-04 18:29:43 +0000

    Implements SkipNode class

---
+++ src/wacc/tree/nodes/SkipNode.java
@@ -0,0 +1,13 @@
+package wacc.tree.nodes;
+
+import wacc.tree.nodeInterfaces.StatNode;
+
+public class SkipNode extends StatNode {
+
+	@Override
+	public void generate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
