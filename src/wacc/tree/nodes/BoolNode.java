6f8b7bea1186ae1f05a1845af2c1cfcfe7f2cb7e
Tom Barwick <tb1414@ic.ac.uk>
2015-12-05 20:08:25 +0000

    Implements code gen for BoolNode

--- src/wacc/tree/nodes/BoolNode.java
+++ src/wacc/tree/nodes/BoolNode.java
@@ -1,6 +1,8 @@
 package wacc.tree.nodes;
 
 import wacc.tree.nodeSupers.ExpNode;
+import wacc.util.Arm;
+import wacc.util.ProgramCode;
 import wacc.util.Reg;
 
 public class BoolNode extends ExpNode {
@@ -13,6 +15,7 @@ public class BoolNode extends ExpNode {
 
     @Override
     public Reg generate() {
-
+    	ProgramCode.add("MOV r4, " + Arm.imm(b ? 1 : 0));
+    	return Reg.R4;
     }
 }
