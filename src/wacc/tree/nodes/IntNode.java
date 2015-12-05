6a87fa636193d89e72651a1f7a762c1338ffa74f
Tom Barwick <tb1414@ic.ac.uk>
2015-12-05 20:08:55 +0000

    Implements code gen for IntNode

--- src/wacc/tree/nodes/IntNode.java
+++ src/wacc/tree/nodes/IntNode.java
@@ -1,6 +1,8 @@
 package wacc.tree.nodes;
 
 import wacc.tree.nodeSupers.ExpNode;
+import wacc.util.Arm;
+import wacc.util.ProgramCode;
 import wacc.util.Reg;
 
 public class IntNode extends ExpNode {
@@ -13,6 +15,7 @@ public class IntNode extends ExpNode {
 
     @Override
     public Reg generate() {
-
+    	ProgramCode.add("LDR r4, " + Arm.imm(n));
+    	return Reg.R4;
     }
 }
