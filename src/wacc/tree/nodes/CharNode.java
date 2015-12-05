acff7f32af1522a6a8c84bac22f9d47f400235f4
Tom Barwick <tb1414@ic.ac.uk>
2015-12-05 20:08:35 +0000

    Implements code gen for CharNode

--- src/wacc/tree/nodes/CharNode.java
+++ src/wacc/tree/nodes/CharNode.java
@@ -1,6 +1,8 @@
 package wacc.tree.nodes;
 
 import wacc.tree.nodeSupers.ExpNode;
+import wacc.util.Arm;
+import wacc.util.ProgramCode;
 import wacc.util.Reg;
 
 public class CharNode extends ExpNode {
@@ -13,6 +15,7 @@ public class CharNode extends ExpNode {
 
     @Override
     public Reg generate() {
-
+    	ProgramCode.add("MOV r4, " + Arm.imm("'c'"));
+    	return Reg.R4;
     }
 }
