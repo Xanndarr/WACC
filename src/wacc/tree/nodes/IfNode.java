1e2e655354209056af557b948b1dd6f0a55f5c96
Tom Barwick <tb1414@ic.ac.uk>
2015-12-06 01:08:46 +0000

    Implements code generation for IfNode

--- src/wacc/tree/nodes/IfNode.java
+++ src/wacc/tree/nodes/IfNode.java
@@ -1,12 +1,30 @@
 package wacc.tree.nodes;
 
 import wacc.tree.nodeSupers.StatNode;
+import wacc.util.ProgramCode;
 import wacc.util.Reg;
 
 public class IfNode extends StatNode {
 
 	@Override
 	public Reg generate() {
+		Reg condRet = children.get(0).generate();
+		ProgramCode.add("CMP " + condRet + ", #0");
+		String thenLabel = ProgramCode.generateUniqueLabel();
+		String elseLabel = ProgramCode.generateUniqueLabel();
+		ProgramCode.add("BEQ " + thenLabel);
+		ProgramCode.add("B " + elseLabel);
+		
+		ProgramCode.setIndent(false);
+		ProgramCode.add(thenLabel + ":");
+		ProgramCode.setIndent(true);
+		children.get(1).generate();
+
+		ProgramCode.setIndent(false);
+		ProgramCode.add(elseLabel + ":");
+		ProgramCode.setIndent(true);
+		children.get(2).generate();
+		
 		return null;
 	}
 
