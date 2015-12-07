c66d34e7b61c8118794524e716fc387230fc815d
Glen Rodgers <gr1614@ic.ac.uk>
2015-12-07 11:20:47 +0000

    Removed now unnecessary null check

--- src/wacc/util/StackHandler.java
+++ src/wacc/util/StackHandler.java
@@ -10,36 +10,33 @@ public class StackHandler {
     //0 represents the index for global scope
     private static int GLOBAL_SCOPE = 0;
     private static int currentScope = GLOBAL_SCOPE;
-    
+
     private static int offset = 0;
 
     private static StackHandler instance;
     private static Map<Integer, SymbolTable<String, StackLocation>> stack;
-    
+
     private StackHandler() {
-    	stack = new HashMap<Integer, SymbolTable<String, StackLocation>>();
-    	stack.put(currentScope, new SymbolTable<String, StackLocation>());
-	}
+        stack = new HashMap<Integer, SymbolTable<String, StackLocation>>();
+        stack.put(currentScope, new SymbolTable<String, StackLocation>());
+    }
 
     public static void add(String ident, StackLocation loc, int size) {
-    	if (instance == null) {
-    		instance = new StackHandler();
-    	}
-    	stack.get(currentScope).put(ident, loc);
+        stack.get(currentScope).put(ident, loc);
         offset += size;
     }
 
     public static StackLocation get(String ident) {
-        for(int i = currentScope; i >= GLOBAL_SCOPE; i--) {
+        for (int i = currentScope; i >= GLOBAL_SCOPE; i--) {
             if (stack.get(i).exists(ident)) {
                 return stack.get(i).get(ident);
             }
         }
         return null;
     }
-    
+
     public static int getOffset() {
-    	return offset;
+        return offset;
     }
 
     public static void descend() {
@@ -48,7 +45,7 @@ public class StackHandler {
     }
 
     public static void ascend() {
-    	stack.remove(currentScope);
+        stack.remove(currentScope);
         currentScope--;
     }
 
