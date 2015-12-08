948ffd170d57f46cb845aa6ff59780bf1ff8ca07
Tom Barwick <tb1414@ic.ac.uk>
2015-12-08 02:27:24 +0000

    Implements code gen for assignment node when lhs is an ident

--- src/wacc/tree/nodes/AssignmentNode.java
+++ src/wacc/tree/nodes/AssignmentNode.java
@@ -1,12 +1,23 @@
 package wacc.tree.nodes;
 
+import wacc.tree.nodeSupers.Node;
 import wacc.tree.nodeSupers.StatNode;
+import wacc.util.ProgramCode;
 import wacc.util.Reg;
+import wacc.util.StackHandler;
 
 public class AssignmentNode extends StatNode {
 
 	@Override
 	public Reg generate() {
+		//TODO other LHSs
+		Node lhs = children.get(0);
+		Reg ret = children.get(1).generate();
+		
+		if (lhs instanceof IdentNode) {
+			String ident = ((IdentNode) lhs).getIdent();
+			ProgramCode.add("STR " + ret + ", " + StackHandler.get(ident));
+		}
 		return null;
 	}
 
