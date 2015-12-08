57ad9ed93c0b9031df1dc68a755b4b04c41edd0a
Glen Rodgers <gr1614@ic.ac.uk>
2015-12-08 01:38:05 +0000

    Implemented free node

--- src/wacc/tree/nodes/FreeNode.java
+++ src/wacc/tree/nodes/FreeNode.java
@@ -1,13 +1,18 @@
 package wacc.tree.nodes;
 
 import wacc.tree.nodeSupers.StatNode;
+import wacc.util.Error;
+import wacc.util.ProgramCode;
 import wacc.util.Reg;
+import wacc.util.RuntimeErrorCode;
 
 public class FreeNode extends StatNode {
 
 	@Override
 	public Reg generate() {
-		return null;
-	}
+		ProgramCode.add("BL p_free_pair");
+        RuntimeErrorCode.addError(Error.NULL_REF);
+        return null;
+    }
 
 }
