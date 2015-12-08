b38d2404ae9ef8d330bfa498d58cf1e20aad9627
Tom Barwick <tb1414@ic.ac.uk>
2015-12-08 07:13:49 +0000

    Fixes imm values being over 1024

--- src/wacc/util/StackLocation.java
+++ src/wacc/util/StackLocation.java
@@ -17,7 +17,12 @@ public class StackLocation {
 		if (offset == 0) {
 			return "[sp]";
 		}
-		return "[sp, " + Arm.imm(-offset) + "]";
+		int temp = offset;
+		while (temp > 1024) {
+			ProgramCode.add("SUB sp, sp, #1024");
+			temp -= 1024;
+		}
+		return "[sp, " + Arm.imm(-temp) + "]";
 	}
 
 }
