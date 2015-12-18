b7203623b99ba62a9291c017ed18255c393eaa47
Glen Rodgers <gr1614@ic.ac.uk>
2015-12-18 01:29:54 +0000

    Adds swap method to allow ++ to be subbed for + etc

--- src/wacc/util/BinaryOp.java
+++ src/wacc/util/BinaryOp.java
@@ -1,20 +1,16 @@
 package wacc.util;
 
 public enum BinaryOp {
-	
-	MULT("*"), DIV("/"), MOD("%"),
-	ADD("+"), SUB("-"),
-	GT(">"), GTE(">="), LT("<"), LTE("<="),
-	EQ("=="), NEQ("!="),
-	AND("&&"), OR("||"),
-	INVALID_OP("");
-	
+
+	MULT("*"), DIV("/"), MOD("%"), ADD("+"), SUB("-"), GT(">"), GTE(">="), LT("<"), LTE("<="), EQ("=="), NEQ("!="), AND(
+			"&&"), OR("||"), INVALID_OP("");
+
 	private String op;
-	
+
 	private BinaryOp(String op) {
 		this.op = op;
 	}
-	
+
 	public static BinaryOp parse(String op) {
 		for (BinaryOp bop : BinaryOp.values()) {
 			if (bop.op.equals(op)) {
@@ -22,6 +18,27 @@ public enum BinaryOp {
 			}
 		}
 		return INVALID_OP;
+	}
+
+	public static BinaryOp swap(String op) {
+		switch (op) {
+		case "++":
+			return ADD;
+		case "--":
+			return SUB;
+		case "+=":
+			return ADD;
+		case "-=":
+			return SUB;
+		case "*=":
+			return MULT;
+		case "/=":
+			return DIV;
+		case "%=":
+			return MOD;
+		default:
+			return INVALID_OP;
+		}
 	}
 
 }
