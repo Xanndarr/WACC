77db29ca96402e6630ef807e154ec3035492e8b6
Glen Rodgers <gr1614@ic.ac.uk>
2015-12-18 15:24:59 +0000

    Allows statnodes to keep track of the last continue and break nodes

--- src/wacc/tree/nodeSupers/StatNode.java
+++ src/wacc/tree/nodeSupers/StatNode.java
@@ -1,3 +1,34 @@
 package wacc.tree.nodeSupers;
 
-public abstract class StatNode extends Node {}
+import wacc.tree.nodes.BreakNode;
+import wacc.tree.nodes.ContinueNode;
+import wacc.util.ProgramCode;
+
+public abstract class StatNode extends Node {
+
+	protected static boolean hasContinue = false;
+	protected static ContinueNode lastContinue;
+	protected static boolean hasBreak = false;
+	protected static BreakNode lastBreak;
+
+	public void checkContinue() {
+		if (hasContinue) {
+			ProgramCode.setIndent(false);
+			lastContinue.createBranch();
+			ProgramCode.setIndent(true);
+		}
+		hasContinue = false;
+		lastContinue = null;
+	}
+
+	public void checkBreak() {
+		if (hasBreak) {
+			ProgramCode.setIndent(false);
+			lastBreak.createBranch();
+			ProgramCode.setIndent(true);
+		}
+		hasBreak = false;
+		lastBreak = null;
+	}
+
+}
